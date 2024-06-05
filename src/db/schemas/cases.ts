d56fc52820bcfe80efb17736ddca529b371e2889
juancruzroldan95 <juancruz.roldan19@gmail.com>
2024-06-05 00:24:09 -0300

    Setup Drizzle

---
+++ src/db/schemas/cases.ts
@@ -0,0 +1,12 @@
+import { boolean, integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+import { users } from "./users";
+
+export const cases = pgTable("cases", {
+  caseId: serial("case_id").primaryKey(),
+  title: text("title"),
+  description: text("description").notNull(),
+  userId: integer("user_id").notNull().references(() => users.userId),
+  createdAt: timestamp("created_at").notNull().defaultNow(),
+  updatedAt: timestamp("updated_at").notNull().defaultNow(),
+  isDeleted: boolean("is_deleted").default(false).notNull(),
+})
